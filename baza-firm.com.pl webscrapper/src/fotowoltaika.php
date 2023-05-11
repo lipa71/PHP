2ff192b81711542c83c2556f8c0d20f60d4f1a00
lipa71 <lipa71sqad@gmail.com>
2023-05-11 13:22:23 +0200

    :)

---
+++ baza-firm.com.pl webscrapper/src/fotowoltaika.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>baza-firm.com.pl webscrapper</title>
+</head>
+<body>
+    <center>
+        <h1>Fotowoltaika</h1>
+        <form method="POST">
+            <select name="region">
+                <option value="" disabled selected>--Wybierz region--</option>
+                <option value="cała_Polska">cała Polska</option>
+            </select>
+            <input type="submit" name="submit" value="Generuj"/>
+        </form>
+        <br>
+        <a href="../index.php">Powrót</a>
+    </center>
+
+    <?php
+        $region = $_POST['region'];
+        if(isset($_POST['submit']) && $region === 'cała_Polska'){
+            include('simple_html_dom.php');
+            include('linki/fotowoltaika/fotowoltaika.php');
+            set_time_limit(0);
+            $wynik_array = [];
+            foreach ($fotowoltaika as $link) {
+                $html = file_get_html($link);
+                $nazwa_firmy = $html->find('div[id="nazwaBox"] h1', 0);
+                $email_link = $html->find('div[class="displayInlineBlock txtLeft"] img', 0);
+                // $telefon = $html->find('p a[href^=tel:]', 0);
+                $url = 'https://www.baza-firm.com.pl' . $email_link->attr['src'];
+                // $file = fopen('fotowoltaika.jpg', 'w');
+                // file_put_contents('fotowoltaika.jpg', file_get_contents($url));
+                // fclose($file); 
+                // $ch = curl_init($url);
+                // $fp = fopen('fotowoltaika.gif', 'wb');
+                // curl_setopt($ch, CURLOPT_FILE, $fp);
+                // curl_setopt($ch, CURLOPT_HEADER, 0);
+                // curl_exec($ch);
+                // curl_close($ch);
+                // fclose($fp);
+                echo $url;
+                copy($url, 'img.gif');
+                break;
+                // if($email == null) {
+                //     $email2 = " ";
+                //     $telefon2 = preg_replace('/tel:/', '', preg_replace('/tabelaFirmaLinktel:/', '', implode($telefon->attr)));
+                // }elseif($telefon == null) {
+                //     $telefon2 = " ";
+                //     $email2 = preg_replace('/mailto:/', '', preg_replace('/tabelaFirmaLinkmailto:/', '', implode($email->attr)));
+                // }else {
+                //     $telefon2 = preg_replace('/tel:/', '', preg_replace('/tabelaFirmaLinktel:/', '', implode($telefon->attr)));
+                //     $email2 = preg_replace('/mailto:/', '', preg_replace('/tabelaFirmaLinkmailto:/', '', implode($email->attr)));
+                // }
+                
+                // $wynik = $nazwa_firmy->plaintext . '#' . $email2 . '#' .  $telefon2;
+                //     array_push($wynik_array, $wynik);
+            }
+
+            // $wynik_array2 = array_unique($wynik_array);
+
+            // foreach ($wynik_array2 as $key => $value) {
+            //     $wynik_array3.=$value. "\n";
+            // }
+
+            // $file = fopen('fotowoltaika.csv', 'w');
+            // file_put_contents('fotowoltaika.csv',$wynik_array3);
+            // fclose($file);      
+        }
+        ?>
+    </body>
+</html>
